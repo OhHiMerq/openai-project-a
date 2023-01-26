2b16aa9af0e997041c1ff79fac45ef1641055e3b
OhHiMerq <dimailigv@gmail.com>
2023-01-26 09:46:35 +0800

    move api key

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 
 import express from 'express'
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors'
 import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-1Y8qWTMKfc3XIcOgaaZAh74J",
-    apiKey:"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",
-});
-const openai = new OpenAIApi(configuration);
 
+dotenv.config()
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 const port = 3080
 
+const configuration = new Configuration({
+  organization: "org-1Y8qWTMKfc3XIcOgaaZAh74J",
+  apiKey:`${process.env.KEY}`,
+});
+const openai = new OpenAIApi(configuration);
+
 app.post('/', async (req,res)=>{
     const {message} = req.body
-    console.log(message)
 
     const response = await openai.createCompletion({
         model: "text-ada-001",
@@ -25,6 +27,7 @@ app.post('/', async (req,res)=>{
         max_tokens: 100,
         temperature: 1,
       });
+
     const mess = response.data.choices[0].text
       console.log("response",mess)
     res.json({message:response.data.choices[0].text})
