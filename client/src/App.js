568e19bcab602e7910bc88890cea7b20c3c3b327
OhHiMerq <dimailigv@gmail.com>
2023-01-26 13:58:36 +0800

    implement ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,169 @@
+import {
+  Box,
+  Button,
+  Group,
+  ScrollArea,
+  Stack,
+  Text,
+  Progress,
+  Textarea,
+  LoadingOverlay,
+  Select,
+  Center,
+  Divider,
+} from "@mantine/core";
+import { useState, useRef, useEffect } from "react";
 
-import {Box, Button, Group, ScrollArea, Stack, Text, Textarea} from '@mantine/core'
-import { useState,useRef, useEffect,} from 'react';
+import { useInputState } from "@mantine/hooks";
 
-import { useInputState } from '@mantine/hooks';
+const ConvoMessage = ({ props }) => {
+  return (
+    <Box
+      sx={(theme) => ({
+        backgroundColor: theme.colors.gray[0],
+        padding: "5px 20px",
+        borderRadius: theme.radius.md,
+      })}
+    >
+      {" "}
+      <Stack spacing={5}>
+        <Text fw={700} color="gray.8">
+          {props.name}
+        </Text>
 
-const ConvoMessage = ({props}) => {
-  return <Box sx={(theme)=>({backgroundColor:theme.colors.gray[0],padding:"5px 20px",borderRadius: theme.radius.md})}> <Stack spacing={5}> 
-  <Text fw={700} color="gray.8">{props.name}</Text>
-  
-  <Text color="gray.8">{props.mess}</Text>
-  </Stack> </Box>
-}
+        <Text color="gray.8">{props.mess}</Text>
+      </Stack>{" "}
+    </Box>
+  );
+};
 
 function App() {
   const viewport = useRef(null);
   const scrollToBottom = () =>
-  viewport.current.scrollTo({ top: viewport.current.scrollHeight, behavior: 'smooth' });
+    viewport.current.scrollTo({
+      top: viewport.current.scrollHeight,
+      behavior: "smooth",
+    });
 
- 
+  const [prompts, setPrompts] = useState("");
+  const [usage, setUsage] = useState({
+    prompt_tokens: 0,
+    completion_tokens: 0,
+    total_tokens: 0,
+  });
+  const [model, setModel] = useState("text-davinci-003");
+  const [responding, setResponding] = useState(false);
+  const [input, setInput] = useInputState("");
+  const [convo, setConvo] = useState([]);
 
-  const [input,setInput] =useInputState('');
-  const [convo,setConvo] = useState([])
+  useEffect(() => {
+    scrollToBottom();
+  }, [convo]);
 
+  useEffect(() => {
+    setPrompts("");
+    setUsage({ prompt_tokens: 0, completion_tokens: 0, total_tokens: 0 });
+  }, [model]);
 
-  useEffect(()=>{
-    scrollToBottom()
-  },[convo])
+  const sendConvo = (name, mess) => {
+    setConvo((c) => [...c, { name, mess }]);
+  };
 
-  const sendConvo = (name,mess)=>{
-    setConvo((c)=> [...c,{name,mess}])
-  }
+  const onSend = async () => {
+    if (!input) return;
+    sendConvo("me", input);
+    setInput("");
+    getResponse(input);
+  };
 
-  const onSend = () => {
-    if (!input) return
-    sendConvo("me",input)
-    setInput('')
-  }
- 
+  const getResponse = (message) => {
+    setResponding(true);
+
+    const p = prompts + "\n" + message;
+
+    fetch("http://localhost:3080/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message: p, model }),
+    })
+      .then((res) => res.json())
+      .then((d) => {
+        sendConvo("Grug", d.message);
+        setPrompts(d.prompt + d.message);
+        setUsage(d.usage);
+        setResponding(false);
+      });
+  };
+  const modelMax = {
+    "text-davinci-003": 4000,
+    "text-curie-001": 2048,
+    "text-babbage-001": 2048,
+    "text-ada-001": 2048,
+  };
   return (
-    <div style={{margin:"50px 100px"}}>
-      <Group position='center' style={{padding:"10px"}}><Text fw={700} fz="xl" >Test Project</Text></Group>
-      <Stack >
-        <ScrollArea style={{  height: 400 }} viewportRef={viewport}>
-        <Stack spacing={"md"}>
-        {convo.map((c)=><ConvoMessage key={c.name} props={c}/>)}
-        </Stack>
+    <div style={{ margin: "50px 100px" }}>
+      <Group position="apart" style={{ padding: "10px" }}>
+        <Text fw={700} fz={22}>
+          OpenAI API Test Project
+        </Text>
+        <Select
+          label="Model"
+          placeholder="select model"
+          value={model}
+          onChange={setModel}
+          data={[
+            { value: "text-davinci-003", label: "text-davinci-003" },
+            { value: "text-curie-001", label: "text-curie-001" },
+            { value: "text-babbage-001", label: "text-babbage-001" },
+            { value: "text-ada-001", label: "text-ada-001" },
+          ]}
+        />
+      </Group>
+      <Stack>
+        <ScrollArea style={{ height: 400 }} viewportRef={viewport}>
+          <Stack spacing={"md"} align={"stretch"}>
+            {convo.length > 0 ? (
+              convo.map((c, i) => <ConvoMessage key={i} props={c} />)
+            ) : (
+              <Center>
+                <Text>Start Chatting</Text>
+              </Center>
+            )}
+          </Stack>
         </ScrollArea>
-        <Group position='center'>
-          <Textarea onKeyDown={(e)=>{if(e.key === 'Enter'){onSend()}}} value={input} style={{width:"80%"}} label="Chat" minRows={3} onChange={setInput}/>
+
+        <Divider />
+        <Group>
+          <Text fw={500}>Prompt Tokens:</Text>
+          <Text>{usage["prompt_tokens"]}</Text>
+          <Text fw={500}>Completion Tokens:</Text>
+          <Text>{usage["completion_tokens"]}</Text>
+          <Text fw={500}>Total Tokens:</Text>
+          <Text>{usage["total_tokens"]}</Text>
+          <Text fw={500}>Max Tokens:</Text>
+          <Text>{modelMax[model]}</Text>
+        </Group>
+
+        <Progress
+          size="xs"
+          value={(usage["total_tokens"] / modelMax[model]) * 100}
+        />
+        <Group position="center">
+          <Text>{input.length}/200</Text>
+          <LoadingOverlay visible={responding} overlayBlur={2} />
+          <Textarea
+            maxLength="200"
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                onSend();
+              }
+            }}
+            value={input}
+            style={{ width: "80%" }}
+            label="Chat"
+            minRows={3}
+            onChange={setInput}
+          />
           <Button onClick={onSend}>Send</Button>
         </Group>
       </Stack>
--- index.js
+++ index.js
@@ -1,39 +1,50 @@
-
-import express from 'express'
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import cors from 'cors'
+import express, { response } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
-dotenv.config()
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const port = 3080
+const port = 3080;
 
 const configuration = new Configuration({
   organization: "org-1Y8qWTMKfc3XIcOgaaZAh74J",
-  apiKey:`${process.env.KEY}`,
+  apiKey: `${process.env.KEY}`,
 });
 const openai = new OpenAIApi(configuration);
 
-app.post('/', async (req,res)=>{
-    const {message} = req.body
+app.post("/", async (req, res) => {
+  const { message, model } = req.body;
 
-    const response = await openai.createCompletion({
-        model: "text-ada-001",
-        prompt: `${message}`,
-        max_tokens: 100,
-        temperature: 1,
-      });
+  const response = await openai.createCompletion({
+    model: `${model}`,
+    prompt: `This conversation is about an AI who is a dog that talks little english
+        me: hi
+        ai: hello there, woof! 
+        
+        me: who are you?
+        ai: ARF! I'am a dog, my name is Grug WOOF!
 
-    const mess = response.data.choices[0].text
-      console.log("response",mess)
-    res.json({message:response.data.choices[0].text})
-})
+        me: what do you like?
+        ai: I like to play fetch! chew my toys and bark WOOF!
+        
+        me: ${message}
+        ai: `,
+    max_tokens: 100,
 
-app.listen(port,()=>{
-    console.log(`Listening at post ${port}`)
-})
+    temperature: 1,
+  });
 
+  const prompt = JSON.parse(response.config.data).prompt;
+  const mess = response.data.choices[0].text;
+
+  res.json({ prompt, message: mess, usage: response.data.usage });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at post ${port}`);
+});
